29e5ca7e84ca10838c5991bde4fbd24abe5013b0
dimaswntech <67653363+dimaswntech@users.noreply.github.com>
2021-12-21 16:06:42 +0700

    Add files via upload

---
+++ tugas10.js
@@ -0,0 +1,23 @@
+// 1. Pastikan Teman Teman Sudah Menginstall Node JS
+
+// 2. Teman - Teman Buatlah File di Folder javascript teman teman sebuah file dengan nama file tugas10.js
+
+// 3. Buatlah Program Dengan Spesifikasi dibawah ini :
+
+// Buatlah sebuah perulangan untuk menampilkan bilangan seperti dibawah ini :
+
+// Hint : Tambahkan 1 variabel penampung.
+
+// 4. Happy Codding :)
+
+var j = 1;
+var x = 2;
+//j++;
+do {
+	console.log(j)
+    
+	
+    j=j+x;
+    //x=x+1;
+    x++;
+}while(j <=55)
